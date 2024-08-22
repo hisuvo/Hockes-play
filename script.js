ad76ec80e9117c4dffcd573aff07ff5a22523cfc
suvo <suvodatta72@gmail.com>
2024-08-22 08:39:30 +0600

    hockes website initial setup done

---
+++ script.js
@@ -0,0 +1,13 @@
+
+tailwind.config = {
+    theme: {
+    extend: {
+        colors: {
+        clifford: '#da373d',
+        },
+        fontFamily:{
+            manrope:`"Manrope", sans-serif`
+        }
+    }
+    }
+}
